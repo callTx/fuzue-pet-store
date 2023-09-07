2cb3de05214415711f7fb8d55f222979f6c20b78
callTx <danielfdeus@gmail.com>
2023-09-07 13:54:27 -0300

    Revert "Remove mais variaveis ProductGroup"
    This reverts commit 133176b37b7ae054967df34ec9fe016ad01f226f.

--- components/UI/MenuItems/MenuItems.tsx
+++ components/UI/MenuItems/MenuItems.tsx
@@ -58,15 +58,13 @@ const MenuItems: React.FC<Props> = (props) => {
                 }`}
                 onClick={() =>
                   onMenuItemClickHandler(
-                    // item.productsGroup,
+                    item.productsGroup,
                     item.category,
                     index
                   )
                 }
                 onMouseOver={() =>
-                  props.onMouseOver?.(
-                    // item.productsGroup, 
-                    index, item.category)
+                  props.onMouseOver?.(item.productsGroup, index, item.category)
                 }
               >
                 <item.icon className="w-6 h-6 " />
@@ -91,14 +89,14 @@ const MenuItems: React.FC<Props> = (props) => {
                   }`}
                   onClick={() =>
                     onMenuItemClickHandler(
-                      // item.productsGroup,
+                      item.productsGroup,
                       item.category,
                       index
                     )
                   }
                   onMouseOver={() =>
                     props.onMouseOver?.(
-                      // item.productsGroup,
+                      item.productsGroup,
                       index,
                       item.category
                     )
