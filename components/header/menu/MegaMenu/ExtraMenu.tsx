b0c849edc24ff19e4a06460ece941bdeaf84ef5d
callTx <danielfdeus@gmail.com>
2023-09-07 15:23:36 -0300

    Comenta ExtraMenu

--- components/header/menu/MegaMenu/ExtraMenu.tsx
+++ components/header/menu/MegaMenu/ExtraMenu.tsx
@@ -7,7 +7,7 @@ const ExtraMenu = () => {
   const { t } = useLanguage();
   return (
     <div className="flex items-center ltr:border-l-2 rtl:border-r-2 border-gray-300 grow rtl:mx-5 ltr:ml-2">
-      {extraMenu.map((menuItem) => {
+      {/* {extraMenu.map((menuItem) => {
         return (
           <div
             className="flex items-center text-base/90 mx-2"
@@ -19,7 +19,7 @@ const ExtraMenu = () => {
             </Link>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
