fdeb462da22b88b0a26006f0116df8ee6e073271
callTx <danielfdeus@gmail.com>
2023-09-07 15:03:45 -0300

    Add categoriaDeProdutos

--- mock/menuItems.js
+++ mock/menuItems.js
@@ -11,25 +11,102 @@ import { FiMonitor, FiHeadphones } from "react-icons/fi";
 import { GiLargeDress } from "react-icons/gi";
 import { FaBaby, FaRedhat } from "react-icons/fa";
 
+// const menuItems = [
+//   {
+//     category: "digital",
+//     icon: BsLaptop,
+//     productsGroup: [
+//       {
+//         title: "laptop",
+//         icon: BsLaptop,
+//         subtitles: [
+//           "asus",
+//           "apple",
+//           "dell",
+//           "lenovo",
+//           "samsung",
+//           "hp",
+//           "huawei",
+//           "acer",
+//           "msi",
+//         ],
+//       },
+//       {
+//         title: "mobile",
+//         icon: ImMobile,
+//         subtitles: [
+//           "samsung",
+//           "apple",
+//           "nokia",
+//           "xiaomi",
+//           "motorola",
+//           "lg",
+//           "sony",
+//         ],
+//       },
+//       {
+//         title: "computer",
+//         icon: FiMonitor,
+//         subtitles: ["monitor", "mouse", "keyboard", "hard"],
+//       },
+//       {
+//         title: "other",
+//         icon: FiHeadphones,
+//         subtitles: ["tablet", "powerBank", "speaker", "headphones"],
+//       },
+//     ],
+//   },
+//   {
+//     category: "fashion",
+//     icon: IoShirtOutline,
+//     productsGroup: [
+//       {
+//         title: "women",
+//         icon: GiLargeDress,
+//         subtitles: [
+//           "dress",
+//           "skirt",
+//           "jeans",
+//           "pants",
+//           "tShirt",
+//           "shoes",
+//           "scarf",
+//         ],
+//       },
+//       {
+//         title: "men",
+//         icon: IoShirtSharp,
+//         subtitles: ["shirt", "pants", "tie", "tShirt", "shoes", "jeans"],
+//       },
+//       {
+//         title: "child",
+//         icon: FaBaby,
+//         subtitles: ["overalls", "mittens", "babyApron", "shoes", "tShirt"],
+//       },
+//       {
+//         title: "other",
+//         icon: FaRedhat,
+//         subtitles: ["watch", "wallet", "hat", "belt"],
+//       },
+//     ],
+//   },
+//   { category: "toys", icon: MdOutlineToys },
+//   { category: "cosmetic", icon: RiHeartPulseLine },
+//   { category: "home", icon: AiOutlineHome },
+//   { category: "sport", icon: BiFootball },
+//   { category: "stationery", icon: BsBook },
+// ];
+
 const menuItems = [
   { category: "alimentoUmido", icon: MdOutlineToys },
-  { category: "cosmetic", icon: RiHeartPulseLine },
-  { category: "home", icon: AiOutlineHome },
-  { category: "sport", icon: BiFootball },
-  { category: "stationery", icon: BsBook },
+  { category: "petiscosNaturais", icon: RiHeartPulseLine },
+  { category: "comedouros", icon: AiOutlineHome },
+  { category: "tapetesParaAlimentacao", icon: BiFootball },
+  { category: "brinquedosEMordedores", icon: BsBook },
+  { category: "pelucias", icon: MdOutlineToys },
+  { category: "kongs", icon: MdOutlineToys },
   { category: "outros", icon: BsBook, productsGroup: [{title: "", icon: "", subtitles: [""]}] }
 ];
-
-// const menuItems = [
-//   { category: "AlimentoÚmido", icon: MdOutlineToys},
-//   { category: "Petiscos Naturais", icon: RiHeartPulseLine  },
-//   { category: "Comedouros", icon: AiOutlineHome  },
-//   { category: "Tapete para Alimentação", icon: BiFootball },
-//   { category: "Brinquedos e Mordedores", icon: BsBook  },
-//   { category: "Pelúcia", icon: BsBook  },
-//   { category: "KONG", icon: BsBook  },
-//   { category: "Outros", icon: BsBook, productsGroup: [{title: "", icon: "", subtitles: [""]}] },
-// ];
 
 export default menuItems;
 
