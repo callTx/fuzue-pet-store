409e98434fac115878bf60ccbb9a1fae94cb4e2f
callTx <danielfdeus@gmail.com>
2023-09-07 14:04:14 -0300

    Add [] ao productGroup

--- mock/menuItems.js
+++ mock/menuItems.js
@@ -98,11 +98,11 @@ import { FaBaby, FaRedhat } from "react-icons/fa";
 // ];
 
 const menuItems = [
-  { category: "toys", icon: MdOutlineToys, productsGroup: ""},
-  { category: "cosmetic", icon: RiHeartPulseLine, productsGroup: "" },
-  { category: "home", icon: AiOutlineHome, productsGroup: "" },
-  { category: "sport", icon: BiFootball, productsGroup: "" },
-  { category: "stationery", icon: BsBook, productsGroup: "" },
+  { category: "toys", icon: MdOutlineToys, productsGroup: []},
+  { category: "cosmetic", icon: RiHeartPulseLine, productsGroup: [] },
+  { category: "home", icon: AiOutlineHome, productsGroup: [] },
+  { category: "sport", icon: BiFootball, productsGroup: [] },
+  { category: "stationery", icon: BsBook, productsGroup: [] },
 ];
 
 export default menuItems;
