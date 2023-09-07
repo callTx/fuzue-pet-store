b872b49e093306b1663b748787ae30320585071e
callTx <danielfdeus@gmail.com>
2023-09-07 15:59:27 -0300

    Add semProdutos

--- components/productList/ProductList.tsx
+++ components/productList/ProductList.tsx
@@ -65,7 +65,7 @@ const ProductList: React.FC<Props> = ({ productList }) => {
             </div>
           </div>
         ) : (
-          <p className="text-palette-mute text-center mt-8">{t.noProduct}</p>
+          <p className="text-palette-mute text-center mt-8">{t.semProdutos}</p>
         )}
       </div>
     </div>
